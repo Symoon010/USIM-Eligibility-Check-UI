301f3a265c73d12f9909a0aa3f00590d70b6bfa9
Symoon Zadid <symoon.bs23@gmail.com>
2021-10-27 11:46:07 +0600

    Initial Commit

---
+++ USIM Eligibility Check UI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  USIM Eligibility Check UI
+//
+//  Created by BS-125 on 27/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
